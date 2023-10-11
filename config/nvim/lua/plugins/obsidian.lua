6b1433fd21480dcd33aae920d908b759aa5483ac
Jonathan Sessions <jon@sessionsdev.com>
2023-10-10 20:59:07 -0400

    removed global user name from gitconfig

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -60,8 +60,8 @@ return {
     -- URL it will be ignored but you can customize this behavior here.
     follow_url_func = function(url)
       -- Open the URL in the default web browser.
-      -- vim.fn.jobstart({"open", url})  -- Mac OS
-      vim.fn.jobstart({ "xdg-open", url }) -- linux
+        vim.fn.jobstart({"open", url})  -- Mac OS
+      --  vim.fn.jobstart({ "xdg-open", url }) -- linux
     end,
   },
 }
