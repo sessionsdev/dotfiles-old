60bd830fb0f3d885902e60ea3f2a2e54f3beac03
Jonathan Sessions <jon@sessionsdev.com>
2023-10-17 01:36:57 -0400

    added startify and indent to neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,35 @@
 vim.g.mapleader = " "
 
+
+
+local g = vim.g
+
+g.startify_custom_header = 'startify#pad(startify#fortune#cowsay())'
+g.webdevicons_enable_startify = 1
+g.startify_enable_special = 1
+g.startify_session_autoload = 1
+g.startify_session_delete_buffers = 1
+g.startify_change_to_vcs_root = 1
+g.startify_fortune_use_unicode = 1
+g.startify_session_persistence = 1
+
+g.startify_lists = {
+  { type = 'files', header = { '   Recent Files' } },
+  { type = 'dir', header = { '   Current Directory '..vim.fn.getcwd()..':' } },
+  { type = 'bookmarks', header = { '   Bookmarks' } },
+  { type = 'commands', header = { '   Commands' } },
+}
+
+g.startify_bookmarks = {
+  { i = '~/git/dotfiles/config/nvim/init.lua'},
+}
+
+g. startify_commands = {
+  {h = ':help reference'}
+}
+
+
+
+
+
 require("core.init")
