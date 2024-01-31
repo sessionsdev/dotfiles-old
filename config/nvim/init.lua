d0cc9fe26ecacbbbcb596fe5e2db96c9113968df
Jon Sessions <jon@sessionsdev.com>
2024-01-30 22:06:13 -0500

    removing dead code

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
 require("core.init")
 require('core.utilities').set_os_globals()
-require('core.auto_commands')
 
--- config/nvim/lua/core/auto_commands.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd [[
-augroup MyWikiAutocmds
-    autocmd!
-    autocmd User WikiBufferInitialized lua require'core.keymap'.map_wiki_keys()
-augroup END
-]]
